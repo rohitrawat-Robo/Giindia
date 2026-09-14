379d66487d6af093a4acdfb866ab9b7d788c4a18
rohitsinghal79 <rohit.singhal@giindia.com>
2026-09-14 10:36:51 +0530

    deployment1

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/dist">
       <App />
     </BrowserRouter>
   </HelmetProvider>
